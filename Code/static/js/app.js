3a096f5523abb2b31c3f968e244f86e7214b2103
Faith-Hall <135525815+Faith-Hall@users.noreply.github.com>
2023-10-15 16:19:29 -0500

    code

--- Code/static/js/app.js
+++ Code/static/js/app.js
@@ -1,8 +1,9 @@
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
 // Use D3 library to read in samples.json from the URL
-
-
+d3.json(url).then(function(data) {
+    console.log(data);
+  });
 // Create a horizontal bar chart with a dropdown menu to display the top 10 OTUs found in that individual
 
 // Display demographic info of subject
